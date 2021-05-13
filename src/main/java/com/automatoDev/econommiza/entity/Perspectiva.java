21ef50d2179ef7d430ff346bea8e9023b818252e
Marco Aurelio <programadormoderador@hotmail.com>
2021-05-13 00:26:13 -0300

    Adjust imports

--- src/main/java/com/automatoDev/econommiza/entity/Perspectiva.java
+++ src/main/java/com/automatoDev/econommiza/entity/Perspectiva.java
@@ -24,7 +24,6 @@ import com.automatoDev.econommiza.entity.validationGroups.ConverterGroup;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
-import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
